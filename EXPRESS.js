36a2bd42abc2f2c895dce456188d7d38d19136a7
Saulo <saulo.queiroz@estudante.ifms.edu.br>
2026-09-24 21:01:37 -0400

    Adicionei middlewares de autenticação, validação e logger.

--- EXPRESS.js
+++ EXPRESS.js
@@ -11,3 +11,50 @@ let tarefas = [
     {id: 3, titulo: 'Lavar roupa', concluida: true}
 ]
 
+let proximoId = tarefas.length + 1
+
+app.get('/', (req, res) => {
+    res.json({Mensagem: 'API de Tarefas no ar!'})
+})
+
+function filtrarPorStatus(req, res, next){
+    const {concluida} = req.query
+
+    if(concluida !== undefined){
+        const statusBooleano = concluida === 'true'
+
+        const tarefasConcluidas = tarefas.filter(
+            tarefa => tarefa.concluida === statusBooleano
+        )
+        
+        return res.json(tarefasConcluidas)
+    }
+
+    next()
+}
+
+app.get('/tarefas', filtrarPorStatus, (req, res) => {
+    res.json(tarefas)
+})
+
+function buscarTarefa(req, res, next){
+    const {id} = req.params
+    const idTarefa = parseInt(id)
+
+    const tarefa = tarefas.find(t => t.id === idTarefa)
+
+    if(!tarefa){
+        return res.status(404).json({
+            Error: 'Tarefa não encontrada!'
+        })
+    }
+
+    req.tarefa = tarefa
+
+    next()
+}
+
+app.get('/tarefas/:id', buscarTarefa, (req, res) => {
+    res.json(req.tarefa)
+})
+
